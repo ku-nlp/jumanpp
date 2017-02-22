bf2bafe1d6acf9beba5791be4d9ed7e01b7473b5
Arseny Tolmachev <eiennohito@gmail.com>
2017-02-22 13:08:20 +0900

    hardcode test running dir

--- v2/src/testing/catch_main.cc
+++ v2/src/testing/catch_main.cc
@@ -2,5 +2,20 @@
 // Created by Arseny Tolmachev on 2017/02/18.
 //
 
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+
+#include "test_config.h"
+
+int main(int argc, char* argv[]) {
+  std::printf("using %s as test resource directory...", TEST_RESOURCE_DIR);
+
+  if (chdir(TEST_RESOURCE_DIR) != 0) {
+    std::printf(" failed: %s", strerror(errno));
+  } else {
+    std::puts("\n");
+  }
+
+  int result = Catch::Session().run(argc, argv);
+  return (result < 0xff ? result : 0xff);
+}
