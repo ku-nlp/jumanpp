358809cad6d700c4295ace1fa93200cd29d91320
Arseny Tolmachev <eiennohito@gmail.com>
2017-02-26 17:13:27 +0900

    improve facilities of memory management

--- v2/src/util/memory.hpp
+++ v2/src/util/memory.hpp
@@ -13,12 +13,25 @@
 
 #include <cstddef>
 #include <vector>
+#include <memory>
+#include "util/common.hpp"
 
 namespace jumanpp {
 namespace util {
 namespace memory {
 
 inline bool IsPowerOf2(size_t val) { return (val & (val - 1)) == 0; }
+inline bool IsAligned(size_t val, size_t alignment) {
+  return (val & (alignment - 1)) == 0;
+}
+
+inline size_t Align(size_t val, size_t alignment) {
+  JPP_DCHECK(IsPowerOf2(alignment));
+  if (!IsAligned(val, alignment)) {
+    return (val & ~(alignment - 1)) + alignment;
+  }
+  return val;
+}
 
 struct MemoryPage {
   void *base;
@@ -26,6 +39,17 @@ struct MemoryPage {
 };
 
 class Manager;
+
+template <typename T>
+struct DestructorOnlyDeleter {
+  using ptr_t = typename std::unique_ptr<T, DestructorOnlyDeleter<T>>::pointer;
+  void operator()(ptr_t ptr) const {
+    ptr->~T(); //call destructor
+  }
+};
+
+template <typename T>
+using ManagedPtr = std::unique_ptr<T, DestructorOnlyDeleter<T>>;
 
 class ManagedAllocatorCore {
   Manager *mgr_;
@@ -36,6 +60,28 @@ class ManagedAllocatorCore {
  public:
   ManagedAllocatorCore(Manager *manager) : mgr_(manager) {}
   void *allocate_memory(size_t size, size_t alignment);
+
+  template <typename T>
+  T *allocate(size_t align) {
+    return (T *)allocate_memory(sizeof(T), align);
+  };
+
+  template <typename T>
+  T *allocateArray(size_t count, size_t align = alignof(T)) {
+    return (T *)allocate_memory(sizeof(T) * count, align);
+  }
+
+  template <typename T, typename... Args>
+  T *make(Args... args) {
+    T *buffer = allocate<T>(alignof(T));
+    auto obj = new (buffer) T(std::forward<Args>(args)...);
+    return obj;
+  };
+
+  template <typename T, typename... Args>
+  ManagedPtr<T> make_unique(Args... args) {
+    return ManagedPtr<T>(make<T, Args...>(std::forward<Args...>(args...)));
+  };
 };
 
 template <typename T, size_t Talign = alignof(T)>
@@ -65,6 +111,10 @@ class Manager {
     return ManagedAllocator<T>{ManagedAllocatorCore{this}};
   }
 
+  ManagedAllocatorCore core() {
+    return ManagedAllocatorCore{this};
+  }
+
   Manager(size_t page_size) : page_size_{page_size} {}
 
 #if JUMANPP_USE_DEFAULT_ALLOCATION
@@ -75,6 +125,33 @@ class Manager {
 
   ~Manager();
 };
+
+template <typename T>
+class StlManagedAlloc {
+  ManagedAllocatorCore* core_;
+public:
+  using value_type = T;
+  using pointer = value_type*;
+
+  StlManagedAlloc(ManagedAllocatorCore* core) noexcept : core_{core} {}
+
+  pointer allocate(size_t n) {
+    return core_->allocateArray<T>(n);
+  }
+
+  void deallocate(T* obj, size_t n) noexcept {
+    //noop!
+  }
+
+  bool operator==(const StlManagedAlloc<T>& o) const noexcept {
+    return core_ == o.core_;
+  }
+
+  bool operator!=(const StlManagedAlloc<T>& o) const noexcept {
+    return !(*this == o);
+  }
+};
+
 }
 }
 }
--- v2/src/util/memory_test.cpp
+++ v2/src/util/memory_test.cpp
@@ -28,10 +28,57 @@ TEST_CASE("powers of 2") {
   CHECK_FALSE(m::IsPowerOf2(231));
 }
 
+TEST_CASE("align works") {
+  CHECK(m::Align(0, 4) == 0);
+  CHECK(m::Align(1, 4) == 4);
+  CHECK(m::Align(2, 4) == 4);
+  CHECK(m::Align(3, 4) == 4);
+  CHECK(m::Align(4, 4) == 4);
+  CHECK(m::Align(5, 4) == 8);
+
+
+  CHECK(m::Align(0, 8) == 0);
+  CHECK(m::Align(4, 8) == 8);
+  CHECK(m::Align(5, 8) == 8);
+  CHECK(m::Align(6, 8) == 8);
+  CHECK(m::Align(7, 8) == 8);
+  CHECK(m::Align(8, 8) == 8);
+  CHECK(m::Align(9, 8) == 16);
+}
+
 TEST_CASE("basic allocator is working", "[memory]") {
   m::Manager mgr{1 << 10};
   auto alloc = mgr.allocator<int>();
   auto intptr = alloc.make(1);
   CHECK(intptr != nullptr);
   CHECK(*intptr == 1);
+}
+
+TEST_CASE("StlManagedAlloc adheres to stl spec") {
+  m::Manager mgr_{1024*1024};
+  auto core1 = mgr_.core();
+  auto core2 = mgr_.core();
+
+  m::StlManagedAlloc<int> a1{&core1};
+  m::StlManagedAlloc<int> a2{&core2};
+  CHECK(a1 != a2);
+
+  m::StlManagedAlloc<int> a3{a2};
+  CHECK(a2 == a3);
+  CHECK(a1 != a3);
+
+  m::StlManagedAlloc<int> a4{std::move(a3)};
+  CHECK(a2 == a4);
+  CHECK(a1 != a4);
+}
+
+TEST_CASE("std::vector works with managedalloc") {
+  m::Manager mgr_{1024*1024};
+  auto core = mgr_.core();
+  std::vector<int, m::StlManagedAlloc<int>> mvec{&core};
+  mvec.push_back(1);
+  mvec.push_back(2);
+  mvec.push_back(3);
+  CHECK(mvec.size() == 3);
+  CHECK(mvec.back() == 3);
 }
