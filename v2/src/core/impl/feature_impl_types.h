6411fb2344f6155e68d69b897a2ceb9c3aacded1
Arseny Tolmachev <eiennohito@gmail.com>
2017-03-03 13:22:31 +0900

    move primitive feature context to feature_impl_types

--- v2/src/core/impl/feature_impl_types.h
+++ v2/src/core/impl/feature_impl_types.h
@@ -7,11 +7,86 @@
 
 #include "core/core_types.h"
 #include "util/array_slice.h"
+#include "core/analysis/extra_nodes.h"
+#include "core/impl/field_reader.h"
+#include "util/status.hpp"
 
 namespace jumanpp {
 namespace core {
 namespace features {
 namespace impl {
+
+class DicListTraversal {
+  dic::impl::IntListTraversal trav_;
+
+public:
+  DicListTraversal(const dic::impl::IntListTraversal& trav) : trav_{trav} {}
+  bool next(i32* result) { return trav_.readOneCumulative(result); }
+};
+
+/**
+ * This showsh which dictionary storage field for a column
+ * should be used for computations
+ */
+enum class LengthFieldSource { Invalid, Strings, Positions };
+
+class PrimitiveFeatureContext {
+  analysis::ExtraNodesContext* extraCtx;
+  dic::FieldsHolder* fields;
+
+public:
+  DicListTraversal traversal(i32 fieldIdx, i32 fieldPtr) const {
+    auto& fld = fields->at(fieldIdx);
+    auto trav = fld.postions.listAt(fieldPtr);
+    return DicListTraversal{trav};
+  }
+
+  i32 providedFeature(EntryPtr entryPtr, u32 index) const {
+    auto node = extraCtx->node(entryPtr);
+    if (node == nullptr ||
+        node->header.type != analysis::ExtraNodeType::Unknown) {
+      return 0;
+    }
+    auto features = node->header.unk.providedValues;
+    return features[index];
+  }
+
+  Status checkFieldType(
+      i32 field, std::initializer_list<spec::ColumnType> columnTypes) const;
+
+  Status checkProvidedFeature(u32 index) const { return Status::Ok(); }
+
+  i32 lengthOf(i32 fieldNum, i32 fieldPtr, LengthFieldSource field) {
+    if (fieldPtr < 0) {
+      return extraCtx->lengthOf(fieldNum, fieldPtr);
+    }
+    auto fld = fields->at(fieldNum);
+    switch (field) {
+      case LengthFieldSource::Positions:
+        return fld.postions.lengthOf(fieldPtr);
+      case LengthFieldSource::Strings:
+        return fld.strings.lengthOf(fieldPtr);
+      default:
+        return -1;
+    }
+  }
+
+  Status setLengthField(i32 fieldNum, LengthFieldSource* field) {
+    auto fld = fields->at(fieldNum);
+    auto type = fld.columnType;
+    if (type == spec::ColumnType::StringList) {
+      *field = LengthFieldSource::Positions;
+    } else if (type == spec::ColumnType::String) {
+      *field = LengthFieldSource::Strings;
+    } else {
+      return Status::InvalidState()
+          << "field " << fld.name << " typed " << fld.columnType
+          << " can not be used for length calculation";
+    }
+
+    return Status::Ok();
+  }
+};
 
 class PrimitiveFeatureData {
   util::ArraySlice<EntryPtr> entries_;
