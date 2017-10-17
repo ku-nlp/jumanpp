980643c83f18c2c3b736e884665e2e70b25e0a8a
Arseny Tolmachev <eiennohito@gmail.com>
2017-10-17 11:58:09 +0900

    update fasthash_quality

--- v2/src/core/devtools/fasthash_quality.cc
+++ v2/src/core/devtools/fasthash_quality.cc
@@ -4,19 +4,21 @@
 
 #include <iostream>
 #include <random>
+#include <array>
+#include "util/array_slice.h"
 #include "util/fast_hash.h"
 #include "util/flatmap.h"
 #include "util/inlined_vector.h"
 #include "util/types.hpp"
+#include "util/stl_util.h"
+#include "util/string_piece.h"
 
 using namespace jumanpp;
 
-struct Trigram {
-  u32 t0;
-  u32 t1;
-  u32 t2;
-
-  explicit Trigram(u32 a1, u32 a2 = ~0, u32 a3 = ~0) : t0{a1}, t1{a2}, t2{a3} {}
+struct NgramInfo {
+  u32 uniCnt = 0;
+  u32 biCnt = 0;
+  u32 triCnt = 0;
 };
 
 using h = jumanpp::util::hashing::FastHash1;
@@ -26,42 +28,105 @@ int main(int argc, char* argv[]) {
   u64 maxBi = 50 * 1000;
   u64 maxTri = 3 * 1000;
 
-  util::FlatMap<u64, util::InlinedVector<Trigram, 1>> data;
+  util::FlatMap<u64, NgramInfo> data;
+  std::array<u32, 256> b0;
+  std::array<u32, 256> b1;
+  std::array<u32, 256> b2;
+  std::array<u32, 256> b3;
+  util::fill(b0, 0);
+  util::fill(b1, 0);
+  util::fill(b2, 0);
+  util::fill(b3, 0);
 
-  auto apply = [&](u64 hc, Trigram tg) {
-    auto& v = data[hc];
-    v.push_back(tg);
+  auto apply = [&](u64 v) {
+    auto bt0 = v & 0xff;
+    b0[bt0] += 1;
+    auto bt1 = (v >> 8) & 0xff;
+    b1[bt1] += 1;
+    auto bt2 = (v >> 16) & 0xff;
+    b2[bt2] += 1;
+    auto bt3 = (v >> 24) & 0xff;
+    b3[bt3] += 1;
+  };
+  
+  auto stats = [](StringPiece name, util::ArraySlice<u32> arr) {
+    u64 sum = 0;
+    u32 min = std::numeric_limits<u32>::max();
+    u32 max = 0;
+    for (auto& i: arr) {
+      sum += i;
+      min = std::min(i, min);
+      max = std::max(i, max);
+    }
+
+    auto avg = sum / arr.size();
+    std::cout << "Stats for " << name << ": min=" << min << ", max=" << max << ", avg=" << avg << ", sum=" << sum
+              << "\ndiff=[";
+    sum = 0;
+    min = std::numeric_limits<u32>::max();
+    max = 0;
+    for (auto&i : arr) {
+      auto diff = static_cast<i64>(i) - static_cast<i64>(avg);
+      auto absdiff = std::abs(diff);
+      std::cout << absdiff << ",";
+      sum += absdiff;
+      min = std::min<u32>(min, absdiff);
+      max = std::max<u32>(max, absdiff);
+    }
+    std::cout << "]\n";
+    avg = sum / arr.size();
+    std::cout << "Diff stats: min=" << min << ", max=" << max << ", avg=" << avg << ", sum=" << sum
+              << "\ndiff=[";
   };
 
   for (u32 i = 0; i < maxUni; ++i) {
-    auto hv = h{0xdead}.mix(i).result();
-    apply(hv, Trigram{i});
+    auto hv = h{}.mix(0xdead).mix(i).result();
+    data[hv].uniCnt += 1;
+    apply(hv);
   }
 
   std::cout << "Sequential!\n";
-  std::cout << "Uni" << data.size() << "/" << maxUni << " "
+  std::cout << "Uni " << data.size() << "/" << maxUni << " "
             << (maxUni - data.size()) << " collisions\n";
+  stats("byte-0", b0);
+  stats("byte-1", b1);
+  stats("byte-2", b2);
+  stats("byte-3", b3);
 
   for (u32 i = 0; i < maxBi; ++i) {
-    auto p = h{0xbeef}.mix(i);
+    auto p = h{}.mix(0xbeef).mix(i);
     for (u32 j = 0; j < maxBi; ++j) {
       auto hv = p.mix(j).result();
-      apply(hv, Trigram{i, j});
+      data[hv].biCnt += 1;
+      apply(hv);
+    }
+    auto ratio = maxBi / 100;
+    if (i % ratio == 0) {
+      std::cout << "\rBigrams: " << i / ratio << "% finished" << std::flush;
     }
   }
 
   auto biTotal = maxUni + maxBi * maxBi;
-  std::cout << "+Uni" << data.size() << "/" << biTotal << " "
+  std::cout << "\r+Bi " << data.size() << "/" << biTotal << " "
             << (biTotal - data.size()) << " collisions\n";
+  stats("byte-0", b0);
+  stats("byte-1", b1);
+  stats("byte-2", b2);
+  stats("byte-3", b3);
 
   for (u32 i = 0; i < maxTri; ++i) {
-    auto p1 = h{0xbabe}.mix(i);
+    auto p1 = h{}.mix(0xbabe).mix(i);
     for (u32 j = 0; j < maxTri; ++j) {
       auto p2 = p1.mix(j);
       for (u32 k = 0; k < maxTri; ++k) {
         auto hv = p2.mix(k).result();
-        apply(hv, Trigram{i, j, k});
+        data[hv].triCnt += 1;
+        apply(hv);
       }
+    }
+    auto ratio = maxTri / 100;
+    if (i % ratio == 0) {
+      std::cout << "\rTrigrams: " << i / ratio << "% finished" << std::flush;
     }
   }
 
@@ -69,41 +134,71 @@ int main(int argc, char* argv[]) {
   std::cout << "+Tri" << data.size() << "/" << total << " "
             << (total - data.size()) << " collisions\n";
 
+  stats("byte-0", b0);
+  stats("byte-1", b1);
+  stats("byte-2", b2);
+  stats("byte-3", b3);
+
   data.clear_no_resize();
+  util::fill(b0, 0);
+  util::fill(b1, 0);
+  util::fill(b2, 0);
+  util::fill(b3, 0);
+
   std::minstd_rand rng{1};
   std::uniform_int_distribution<u32> dst;
 
   for (u32 i = 0; i < maxUni; ++i) {
-    auto hv = h{0xdead}.mix(dst(rng)).result();
-    apply(hv, Trigram{i});
+    auto hv = h{}.mix(0xdead).mix(dst(rng)).result();
+    data[hv].uniCnt += 1;
   }
 
   std::cout << "Random!\n";
-  std::cout << "Uni" << data.size() << "/" << maxUni << " "
+  std::cout << "Uni " << data.size() << "/" << maxUni << " "
             << (maxUni - data.size()) << " collisions\n";
+  stats("byte-0", b0);
+  stats("byte-1", b1);
+  stats("byte-2", b2);
+  stats("byte-3", b3);
 
   for (u32 i = 0; i < maxBi; ++i) {
-    auto p = h{0xbeef}.mix(dst(rng));
+    auto p = h{}.mix(0xbeef).mix(dst(rng));
     for (u32 j = 0; j < maxBi; ++j) {
       auto hv = p.mix(dst(rng)).result();
-      apply(hv, Trigram{i, j});
+      data[hv].biCnt += 1;
+    }
+    auto ratio = maxBi / 100;
+    if (i % ratio == 0) {
+      std::cout << "\rBigrams: " << i / ratio << "% finished" << std::flush;
     }
   }
 
-  std::cout << "+Uni" << data.size() << "/" << biTotal << " "
+  std::cout << "\r+Bi " << data.size() << "/" << biTotal << " "
             << (biTotal - data.size()) << " collisions\n";
+  stats("byte-0", b0);
+  stats("byte-1", b1);
+  stats("byte-2", b2);
+  stats("byte-3", b3);
 
   for (u32 i = 0; i < maxTri; ++i) {
-    auto p1 = h{0xbabe}.mix(dst(rng));
+    auto p1 = h{}.mix(0xbabe).mix(dst(rng));
     for (u32 j = 0; j < maxTri; ++j) {
       auto p2 = p1.mix(dst(rng));
       for (u32 k = 0; k < maxTri; ++k) {
         auto hv = p2.mix(dst(rng)).result();
-        apply(hv, Trigram{i, j, k});
+        data[hv].triCnt += 1;
       }
+    }
+    auto ratio = maxTri / 100;
+    if (i % ratio == 0) {
+      std::cout << "\rTrigrams: " << i / ratio << "% finished" << std::flush;
     }
   }
 
-  std::cout << "+Tri" << data.size() << "/" << total << " "
+  std::cout << "\r+Tri " << data.size() << "/" << total << " "
             << (total - data.size()) << " collisions\n";
+  stats("byte-0", b0);
+  stats("byte-1", b1);
+  stats("byte-2", b2);
+  stats("byte-3", b3);
 }
