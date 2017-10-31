1c79fad90de7a5cea45023acf116d59150c3a1c7
Arseny Tolmachev <eiennohito@gmail.com>
2017-10-31 19:25:20 +0900

    use git-clang-format for formatting

--- v2/do_format.sh
+++ v2/do_format.sh
@@ -1,3 +1,6 @@
 #/bin/sh
 
-find -E . -regex '.+\.(cc|cpp|h|hpp)' -print0 | xargs -0 clang-format -i
+#find -E . -regex '.+\.(cc|cpp|h|hpp)' -print0 | xargs -0 clang-format -i
+git add .
+exec python $(dirname $0)/../script/git-clang-format.py $*
+git add .
